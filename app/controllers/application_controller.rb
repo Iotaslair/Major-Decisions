3eab8b745116f910b4835ebfdb43c09390d6a839
Shane Archuleta <shane.archuleta17@ncf.edu>
2019-04-10 14:44:26 -0400

    Flash to user if they are not authorized for a page or action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
   include Pundit
   protect_from_forgery
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   protected
     def configure_permitted_parameters
@@ -12,5 +14,12 @@ class ApplicationController < ActionController::Base
         new_user_params.permit(:email, :firstname, :lastname, :faculty, :password, :password_confirmation)
       end
     end
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || root_path)
+  end
 
 end
