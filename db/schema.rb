b4c7a379accbec292732623ce16cefd598451863
Shane Archuleta <shane.archuleta17@ncf.edu>
2019-04-10 18:14:07 -0400

    db:migrate after new migration for prereqs

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_10_180917) do
+ActiveRecord::Schema.define(version: 2019_04_10_184840) do
 
   create_table "courses", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "prereqs_id"
+    t.index ["prereqs_id"], name: "index_courses_on_prereqs_id"
   end
 
   create_table "users", force: :cascade do |t|
