75819812e257ff16c01f364e3738dc033f1d68cf
Shane Archuleta <shane.archuleta17@ncf.edu>
2019-04-10 10:28:32 -0400

    Add custom /signin, /signout/, and /signup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+
   devise_for :users
+
+  # custom signin, signout, and signup routes for devise
+  devise_scope :user do
+    get 'signin', to: 'devise/sessions#new'
+    get 'signout', to: 'devise/sessions#destroy'
+    get 'signup', to: 'devise/registrations#new'
+  end
+
   root to: 'home#index'
+
   get 'home/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
