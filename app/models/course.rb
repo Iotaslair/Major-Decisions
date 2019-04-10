d193fde616aed73bc94b0a236fbab0fc29a8b4b4
Shane Archuleta <shane.archuleta17@ncf.edu>
2019-04-10 18:14:48 -0400

    Initial prereqs relationships, require unique course titles

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,8 @@
 class Course < ApplicationRecord
 
-  validates :title, presence: true
+  has_many :prereqs, class_name: "Course", foreign_key: "prereqs_id"
+  belongs_to :parent, class_name: "Course", optional: true
+
+  validates :title, presence: true, uniqueness: true
 
 end
