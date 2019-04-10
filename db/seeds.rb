c2e18afd3fed590ffbdd294d88be28a2a0b6fe5a
Shane Archuleta <shane.archuleta17@ncf.edu>
2019-04-10 14:46:50 -0400

    Seed with faculty, student, and some courses

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create a faculty member
+User.create(email: "faculty@ncf.edu", password: "password", faculty: true, firstname: "John", lastname: "Doe")
+
+# Create a student
+User.create(email: "shane@ncf.edu", password: "password", faculty: false, firstname: "Shane", lastname: "A")
+
+# Create some courses
+Course.create(title: "Philosophy of science")
+Course.create(title: "Philosophy of language")
+Course.create(title: "Advanced Ethics")
+Course.create(title: "Virtue and Vices")
