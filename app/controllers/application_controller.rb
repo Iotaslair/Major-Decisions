cdea7507bcf229f2e070b2f4a7091bcf85c444d5
Shane Archuleta <shane.archuleta17@ncf.edu>
2019-04-10 13:23:49 -0400

    Initial configure_permitted_parameters for custom sign_up fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   include Pundit
   protect_from_forgery
+
+  protected
+    def configure_permitted_parameters
+      # Note: This must reference the devise path--not my own custom route
+      devise_parameter_sanitizer.permit(:sign_up) do |new_user_params|
+        new_user_params.permit(:email, :firstname, :lastname, :faculty, :password, :password_confirmation)
+      end
+    end
 
 end
