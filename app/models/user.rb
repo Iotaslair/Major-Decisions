58177eccf036d34964d5268c9d1be565785e53d0
Shane Archuleta <shane.archuleta17@ncf.edu>
2019-04-10 10:47:48 -0400

    Require presence of first and last names

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Require presence of first and last name
+  validates :firstname, presence: true
+  validates :lastname, presence: true
 end
