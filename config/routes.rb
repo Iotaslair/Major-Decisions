a0192a7b02012e12b51b16d26068c3e5a14c5a8e
Shane Archuleta <shane.archuleta17@ncf.edu>
2019-04-11 10:32:27 -0400

    Reorganise, set up routing for user/:id

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'user/show'
-  resources :courses
+  root to: 'home#index'
+
+  # User
+  get 'user/:id', to: 'user#show', as: :user
   devise_for :users
 
   # custom signin, signout, and signup routes for devise
@@ -11,9 +13,8 @@ Rails.application.routes.draw do
     get 'signup', to: 'devise/registrations#new'
   end
 
-  root to: 'home#index'
-
-  get 'home/index'
+  # Courses
+  resources :courses
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
