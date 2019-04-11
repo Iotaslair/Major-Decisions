60eb905d336459236bf583f1501adc56d2060bd2
Shane Archuleta <shane.archuleta17@ncf.edu>
2019-04-11 10:33:00 -0400

    Only show profile page if current user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,13 @@
 class UserController < ApplicationController
+
   def show
+    @user = User.find(params[:id])
+
+    # Make sure that user is viewing their own page
+    if @user != current_user
+      flash[:alert] = "You do not have permission to view this page."
+      redirect_to(request.referrer || root_path)
+    end
   end
+
 end
