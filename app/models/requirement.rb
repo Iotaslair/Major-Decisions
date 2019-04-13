db278399550613e73cf1af6b465c5a348c330723
Shane Archuleta <shane.archuleta17@ncf.edu>
2019-04-13 10:52:27 -0400

    Add validation for required name, has_many course_requirements, has_many courses through course_requirements, belongs_to major

--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -1,3 +1,8 @@
 class Requirement < ApplicationRecord
+
   belongs_to :major
+  has_many :course_requirements
+  has_many :courses, through: :course_requirements
+
+  validates :name, presence: true
 end
