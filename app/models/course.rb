0d15a1866ecb6365de3689dcb2a4112539d928bf
Shane Archuleta <shane.archuleta17@ncf.edu>
2019-04-13 11:16:00 -0400

    has_ancestry

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,8 @@
 class Course < ApplicationRecord
 
+  # Use ancestry gem for handling prereqs
+  has_ancestry
+
   # Join table for linking to many possible requirement areas
   has_many :course_requirements
   has_many :requirements, through: :course_requirements
