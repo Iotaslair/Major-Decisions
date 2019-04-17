07d0cb029d1dfb3ad33538467c216124218fe52c
Shane Archuleta <shane.archuleta17@ncf.edu>
2019-04-17 18:46:02 -0400

    Initial policy

--- app/policies/course_policy.rb
+++ app/policies/course_policy.rb
@@ -1,10 +1,30 @@
 class CoursePolicy < ApplicationPolicy
 
+  # Students can ONLY be able to view (show) courses.
+  # Faculty can create and edit/update courses.
+  # Administrators can destroy courses.
+
   def new?
     create?
   end
 
+  def edit?
+    create?
+  end
+
+  def update?
+    create?
+  end
+
   def create?
-    return user.faculty?
+    if user.faculty? == false
+      @error_message = "You must be a faculty member to modify courses."
+      return false
+    end
+  end
+
+  def destroy?
+    @error_message = "Courses cannot currently be deleted."
+    return false
   end
 end
