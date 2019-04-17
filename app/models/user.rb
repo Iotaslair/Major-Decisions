1831aefa400f500cef6aa162be20e3c0069ef297
Shane Archuleta <shane.archuleta17@ncf.edu>
2019-04-17 10:05:06 -0400

    User has_many :completed_courses

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :completed_courses
+
   validates :email, presence: true, uniqueness: true  # Require a unique email
   validates :firstname, presence: true                # Require a first name
   validates :lastname, presence: true                 # Require a last name
