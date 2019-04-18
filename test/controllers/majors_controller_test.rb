6ac6e25be5616d3198bed911ace2b136f7ceffe2
Shane Archuleta <shane.archuleta17@ncf.edu>
2019-04-18 19:18:25 -0400

    Some simple, sample tests for the majors_controller

--- test/controllers/majors_controller_test.rb
+++ test/controllers/majors_controller_test.rb
@@ -1,48 +1,84 @@
 require 'test_helper'
 
 class MajorsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
-    @major = majors(:one)
+    @major = majors(:computer_science)
+    @student = users(:student)
+    @faculty = users(:faculty)
   end
 
-  test "should get index" do
+  test "should get index if logged in" do
+    sign_in @student
+
     get majors_url
     assert_response :success
   end
 
-  test "should get new" do
+  test "should get new if faculty" do
+    sign_in @faculty
+
     get new_major_url
     assert_response :success
   end
 
-  test "should create major" do
-    assert_difference('Major.count') do
-      post majors_url, params: { major: {  } }
+  test "should not create major if not signed in" do
+    assert_no_difference('Major.count') do
+      post majors_url, params: { major: { title: "Hehehe" }}
     end
-
-    assert_redirected_to major_url(Major.last)
   end
 
-  test "should show major" do
+  test "should not create major if student" do
+    sign_in @student
+
+    assert_no_difference('Major.count') do
+      post majors_url, params: { major: { title: "Hahaha" }}
+    end
+  end
+
+  test "should create major if faculty" do
+    sign_in @faculty
+
+    assert_difference('Major.count') do
+      post majors_url, params: { major: { title: "Philosophy" } }
+    end
+
+    #assert_redirected_to major_url(Major.last)
+  end
+
+  test "should show major if logged in" do
+    sign_in @student
+
     get major_url(@major)
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should get edit if faculty" do
+    sign_in @faculty
+
     get edit_major_url(@major)
     assert_response :success
   end
 
-  test "should update major" do
-    patch major_url(@major), params: { major: {  } }
-    assert_redirected_to major_url(@major)
+  test "should update major if faculty" do
+    sign_in @faculty
+
+    patch major_url(@major), params: { major: { title: "New Computer Science" } }
+    #assert_redirected_to major_url(@major)
   end
 
-  test "should destroy major" do
-    assert_difference('Major.count', -1) do
+  test "should never destroy major" do
+    sign_in @faculty
+
+    # assert_difference('Major.count', -1) do
+    #   delete major_url(@major)
+    # end
+
+    assert_no_difference('Major.count') do
       delete major_url(@major)
     end
 
-    assert_redirected_to majors_url
+    #assert_redirected_to majors_url
   end
 end
