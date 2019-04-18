9a4fc66241703f4ece8b62cf20c1874a0676955d
Shane Archuleta <shane.archuleta17@ncf.edu>
2019-04-18 19:18:06 -0400

    Add test should get index at root

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,8 +1,10 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get home_index_url
+  test "should get index at root" do
+
+    # The root is home#index
+    get root_url
     assert_response :success
   end
 
