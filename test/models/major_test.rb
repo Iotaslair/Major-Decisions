ef9275996dd3bdbb15e7a2cb5389db4bb00b142d
Shane Archuleta <shane.archuleta17@ncf.edu>
2019-04-18 18:45:29 -0400

    Test presence of title

--- test/models/major_test.rb
+++ test/models/major_test.rb
@@ -4,4 +4,14 @@ class MajorTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not save major without title" do
+    major = Major.new
+    assert_not major.save, "Saved the new major without a title."
+  end
+
+  test "should save major with title" do
+    major = Major.new(title: "Philosophy")
+    assert major.save
+  end
 end
