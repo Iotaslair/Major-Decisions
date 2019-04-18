0764009fc7764d2c6d0da1bef0c7343dd6083747
Shane Archuleta <shane.archuleta17@ncf.edu>
2019-04-18 18:45:15 -0400

    Test presence of title, presence of requirement area, duplicates of same requirement area for a given course

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -4,4 +4,23 @@ class CourseTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not save course without title" do
+    course = Course.new
+    assert_not course.save
+  end
+
+  test "should not save course without at least one associated requirement area" do
+    course = Course.new(title: "Calculus 1")
+    assert_not course.save
+  end
+
+  test "should not add same requirement area to same course" do
+    course = Course.new(title: "A course with a unique title123")
+
+    course.requirements << requirements(:comp_sci_maths)
+    course.requirements << requirements(:comp_sci_maths)
+
+    assert_not course.save
+  end
 end
