eec5320502bf450b9391de57b0378fd8edd4fff4
Iotaslair <william.pembleton17@ncf.edu>
2019-04-21 20:30:09 -0400

    Made a setup file for student and faculty (copied from test/controller/major)

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserControllerTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @student = users(:student)
+    @faculty = users(:faculty)
+  end
+
   test "should get show" do
+    sign_in @student
+
+
+
+
     get user_show_url
     assert_response :success
   end
