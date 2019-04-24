400707f33cd3d03d9f771c2ef7801b265e89e522
Iotaslair <william.pembleton17@ncf.edu>
2019-04-24 12:58:15 -0400

    Broken create course test, show course tests work, edit course tests work, update course tests in progress

--- test/controllers/courses_controller_test.rb
+++ test/controllers/courses_controller_test.rb
@@ -23,30 +23,99 @@ class CoursesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get new" do
+  test "should not get new as student" do
+    sign_in @student
+
+    get new_course_url
+    assert_response :redirect
+  end
+
+  test "should get new as faculty" do
+    sign_in @faculty
+
     get new_course_url
     assert_response :success
   end
 
-  test "should create course" do
+  #TODO same as below, insert answer up here
+  test "should not create course as student" do
+    sign_in @student
+
     assert_difference('Course.count') do
-      post courses_url, params: { course: { description: @course.description, title: @course.title } }
+      post courses_url, params: { course: { title: @course.title, description: @course.description } }
     end
 
     assert_redirected_to course_url(Course.last)
   end
 
-  test "should show course" do
+
+  #TODO Fix this test, having trouble with requirement being set correctly
+  test "should create course as faculty" do
+    sign_in @faculty
+
+    assert_difference('Course.count') do
+      post courses_url, params: { course: { title: @course.title, description: @course.description, requirements: 1, requirement: 1  } }
+    end
+
+    assert_redirected_to course_url(Course.last)
+  end
+
+
+  test "should not show course when not logged in" do
+    get course_url(@course)
+    assert_response :redirect
+  end
+
+  test "should show course when logged in as student" do
+    sign_in @student
+
     get course_url(@course)
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should show course when logged in as faculty" do
+    sign_in @student
+
+    get course_url(@course)
+    assert_response :success
+  end
+
+
+
+  test "should not get edit course when not logged in" do
+    get edit_course_url(@course)
+    assert_response :redirect
+  end
+
+  test "should not get edit course when logged in as student" do
+    sign_in @student
+
+    get edit_course_url(@course)
+    assert_response :redirect
+  end
+
+  test "should get edit course when logged in as faculty" do
+    sign_in @faculty
+
     get edit_course_url(@course)
     assert_response :success
   end
 
-  test "should update course" do
+  test "should not update course when not logged in" do
+    patch course_url(@course), params: { course: { description: @course.description, title: @course.title } }
+    assert_response :redirect
+  end
+
+  test "should not update course when logged in as student" do
+    sign_in @student
+
+    patch course_url(@course), params: { course: { description: @course.description, title: @course.title } }
+    assert_response :redirect
+  end
+
+  test "should not update course when logged in as faculty" do
+    sign_in @faculty
+
     patch course_url(@course), params: { course: { description: @course.description, title: @course.title } }
     assert_redirected_to course_url(@course)
   end
