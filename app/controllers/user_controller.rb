c597c13df6bb4d2007c19452da952da492270191
Shane Archuleta <shane.archuleta17@ncf.edu>
2019-04-27 10:26:40 -0400

    Get the user's declared_program if there is one

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,6 +6,12 @@ class UserController < ApplicationController
   def show
     @user = User.find(params[:id])
 
+    # Get the declared program
+    @declared_program = nil
+    if @user.declared_programs.first
+      @declared_program = @user.declared_programs.first.major
+    end
+
     # Make sure that user is viewing their own page
     if @user != current_user
       flash[:alert] = "You do not have permission to view this page."
