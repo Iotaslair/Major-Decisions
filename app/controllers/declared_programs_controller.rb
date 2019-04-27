f64975832fe07c222910e6f7d189e81272cc7de3
Shane Archuleta <shane.archuleta17@ncf.edu>
2019-04-27 10:38:51 -0400

    Authorize against @declared_program, require user to be logged in,

--- app/controllers/declared_programs_controller.rb
+++ app/controllers/declared_programs_controller.rb
@@ -1,4 +1,6 @@
 class DeclaredProgramsController < ApplicationController
+
+  before_action :authenticate_user!
   before_action :set_declared_program, only: [:show, :edit, :update, :destroy]
 
   # GET /declared_programs
@@ -10,15 +12,20 @@ class DeclaredProgramsController < ApplicationController
   # GET /declared_programs/1
   # GET /declared_programs/1.json
   def show
+    authorize @declared_program
   end
 
   # GET /declared_programs/new
   def new
     @declared_program = DeclaredProgram.new
+    @declared_program.user = current_user
+
+    authorize @declared_program
   end
 
   # GET /declared_programs/1/edit
   def edit
+    authorize @declared_program
   end
 
   # POST /declared_programs
@@ -28,7 +35,7 @@ class DeclaredProgramsController < ApplicationController
 
     respond_to do |format|
       if @declared_program.save
-        format.html { redirect_to @declared_program, notice: 'Declared program was successfully created.' }
+        format.html { redirect_to @declared_program, notice: 'You successfully declared an academic program.' }
         format.json { render :show, status: :created, location: @declared_program }
       else
         format.html { render :new }
@@ -42,7 +49,7 @@ class DeclaredProgramsController < ApplicationController
   def update
     respond_to do |format|
       if @declared_program.update(declared_program_params)
-        format.html { redirect_to @declared_program, notice: 'Declared program was successfully updated.' }
+        format.html { redirect_to @declared_program, notice: 'You successfully updated your declared academic program.' }
         format.json { render :show, status: :ok, location: @declared_program }
       else
         format.html { render :edit }
@@ -54,6 +61,8 @@ class DeclaredProgramsController < ApplicationController
   # DELETE /declared_programs/1
   # DELETE /declared_programs/1.json
   def destroy
+    authorize @declared_program
+
     @declared_program.destroy
     respond_to do |format|
       format.html { redirect_to declared_programs_url, notice: 'Declared program was successfully destroyed.' }
