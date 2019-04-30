7cc89258a72d388d191f7c949d7be46b134b86e2
Chief Nelson <chief@Chiefs-MacBook-Pro.local>
2019-04-29 21:57:09 -0400

    Courses left for major calculated
    courses_left stores the number of courses left until a user completes their chosen major. Frontend view still needs to be updated.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -8,8 +8,26 @@ class UserController < ApplicationController
 
     # Get the declared program
     @declared_program = nil
+    @courses_left = nil
+
     if @user.declared_programs.first
       @declared_program = @user.declared_programs.first.major
+
+      # Track degree progress
+
+      # Courses belonging to major
+      @major_courses = Requirement.where(major_id: @declared_program.major_id).courses
+
+      # Courses completed by user
+      @completed_courses = @user.completed_courses
+
+      # Courses still needed for major
+      @courses_left = []
+      for course in @major_courses
+        if !@completed_courses.include? course
+          @courses_left.push(course)
+        end
+      end
     end
 
     # Make sure that user is viewing their own page
