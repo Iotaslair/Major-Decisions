72eeb4917c6e54e820f65f1d5c5d9fa41e50ded6
Iotaslair <william.pembleton17@ncf.edu>
2019-04-30 17:41:43 -0400

    Fixed completed courses show

--- app/controllers/completed_courses_controller.rb
+++ app/controllers/completed_courses_controller.rb
@@ -42,15 +42,15 @@ class CompletedCoursesController < ApplicationController
 
         # Redirect to the user's declared program if one exists
         if current_user.declared_programs.first
-          format.html { redirect_to major_path(current_user.declared_programs.first), notice: 'Course was marked as complete.' }
+          format.html {redirect_to major_path(current_user.declared_programs.first), notice: 'Course was marked as complete.'}
         else
 
           # Else, go to their completed courses list
-          format.html { redirect_to completed_courses_path, notice: 'Course was marked as complete.' }
+          format.html {redirect_to completed_courses_path, notice: 'Course was marked as complete.'}
         end
 
         # JSON stuff
-        format.json { render :show, status: :created, location: @completed_course }
+        format.json {render :show, status: :created, location: @completed_course}
       else
         format.html {render :new}
         format.json {render json: @completed_course.errors, status: :unprocessable_entity}
@@ -84,8 +84,13 @@ class CompletedCoursesController < ApplicationController
   end
 
   private
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def completed_course_params
-      params.require(:completed_course).permit(:user_id, :course_id)
-    end
+
+  def set_completed_course
+    @completed_course = CompletedCourse.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def completed_course_params
+    params.require(:completed_course).permit(:user_id, :course_id)
+  end
 end
